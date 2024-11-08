14c5bb5923fd5b6314968f4ab83382edf8a955f8
Oskar Høxbroe <oskar.hoxbroe@gmail.com>
2024-11-08 11:58:10 +0100

    Made them multicast with several receivers possible.

---
+++ src/Sprint4/Uppgift4b/WeatherReceiver.java
@@ -0,0 +1,40 @@
+package Sprint4.Uppgift4b;
+
+import java.io.IOException;
+import java.net.*;
+
+public class WeatherReceiver {
+
+    WeatherReceiver() {
+        try {
+            //Create socket, IP, group and network interface and then the receiver's socket joins the group
+            int port = 44446;
+            MulticastSocket ms = new MulticastSocket(port);
+            byte[] bytes = new byte[256];
+            InetAddress ip = InetAddress.getByName("234.234.234.234"); //Made up IP within allowed range
+            InetSocketAddress group = new InetSocketAddress(ip, port); //Provided the group with IP and port number
+            NetworkInterface netIf = NetworkInterface.getByName("en0"); //Provided the name of the local network interface
+            ms.joinGroup(group, netIf);
+
+            //Receive all DatagramPackets and convert to String
+            while (true) {
+                DatagramPacket dgp = new DatagramPacket(bytes, bytes.length);
+                ms.receive(dgp);
+                System.out.println("Received from IP " + dgp.getAddress() + ":");
+                String s = new String(dgp.getData(), 0, dgp.getLength());
+                System.out.println(s);
+
+                //Ends the program on the user's request
+                if (s.equalsIgnoreCase("exit"))
+                    System.exit(0);
+            }
+        } catch (IOException e) {
+            System.out.println("There was an error receiving the quotes.");
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        WeatherReceiver wr = new WeatherReceiver();
+    }
+}
---
+++ src/Sprint4/Uppgift4b/WeatherSensor.java
@@ -0,0 +1,54 @@
+package Sprint4.Uppgift4b;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.util.Scanner;
+
+public class WeatherSensor {
+
+    WeatherSensor() {
+        try {
+            //Create IP, port, socket and logic
+            InetAddress ip = InetAddress.getByName("234.234.234.234"); //Made up IP within allowed range
+            int toPort = 44446;
+            MulticastSocket ms = new MulticastSocket();
+            Scanner input = new Scanner(System.in);
+            boolean keepAsking = true;
+
+            //User enters city and current temperature
+            while (keepAsking) {
+                System.out.println("What city are you in?:");
+                String city = input.nextLine();
+                System.out.println("What is the current temperature?:");
+                String temperature = input.nextLine();
+                String cityAndTemperature = "The city is: " + city + ". And it is " + temperature + " degrees right now.";
+
+                //Convert to a byte-array and send as a DatagramPacket
+                byte[] bytes = cityAndTemperature.getBytes();
+                DatagramPacket dgp = new DatagramPacket(bytes, bytes.length, ip, toPort);
+                ms.send(dgp);
+
+                //Exit this program and the receiver program or add more cities
+                System.out.println("Enter 'exit' to exit the program, or press enter to add more cities:");
+                String exitInput = input.nextLine();
+                if (exitInput.equalsIgnoreCase("exit")) {
+                    keepAsking = false;
+                    byte[] exitBytes = exitInput.getBytes();
+                    DatagramPacket dgpExit = new DatagramPacket(exitBytes, exitBytes.length, ip, toPort);
+                    ms.send(dgpExit);
+                }
+            }
+
+        } catch (IOException e) {
+            System.out.println("There was an error sending the quotes.");
+            e.printStackTrace();
+        }
+        System.exit(0);
+    }
+
+    public static void main(String[] args) {
+        WeatherSensor ws = new WeatherSensor();
+    }
+}
